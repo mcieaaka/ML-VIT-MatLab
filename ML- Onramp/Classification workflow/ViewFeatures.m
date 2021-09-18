46b25d64d42c19456e62f4733dd8e003ab957fdb
Harshit <harshit0300@gmail.com>
2021-09-19 00:33:30 +0530

    ml-onramp1

---
+++ ML- Onramp/Classification workflow/ViewFeatures.m
@@ -0,0 +1,16 @@
+%View Features
+%Instructions are in the task pane to the left. Complete and submit each task one at a time.
+
+%Do not edit. This code loads the data.
+load featuredata.mat
+features
+
+%Task 1
+scatter(features.AspectRatio,features.Duration)
+
+
+%Task 2
+gscatter(features.AspectRatio,features.Duration,features.Character)
+
+
+%Further Practice
